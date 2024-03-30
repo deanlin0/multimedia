aa2aa83db7dd754ddfe777fde3a3e81a3f487711
Dean Lin <richer6605@gmail.com>
2024-03-30 19:53:25 +0800

    Add DCT functions and FFTW binding

---
+++ fftw/fftw.go
@@ -0,0 +1,60 @@
+package fftw
+
+// #cgo LDFLAGS: -lfftw3
+// #include <fftw3.h>
+import "C"
+import "unsafe"
+
+var (
+	FFTWREDFT10 = FFTType{
+		cKind: C.FFTW_REDFT10,
+	}
+)
+
+var (
+	FFTWESTIMATE = Flag{
+		cFlag: C.FFTW_ESTIMATE,
+	}
+)
+
+type FFTType struct {
+	cKind C.fftw_r2r_kind
+}
+
+type Flag struct {
+	cFlag C.uint
+}
+
+type Plan struct {
+	cPlan C.fftw_plan
+}
+
+type Array struct {
+	cArray *C.double
+}
+
+func NewArray(data []float64) *Array {
+	return &Array{
+		cArray: (*C.double)(unsafe.Pointer(&data[0])),
+	}
+}
+
+func NewPlan(n int, in, out *Array, fftType FFTType, flag Flag) *Plan {
+	cPlan := C.fftw_plan_r2r_1d(C.int(n), in.cArray, out.cArray, fftType.cKind, flag.cFlag)
+	return &Plan{cPlan}
+}
+
+func ExecutePlan(plan *Plan) {
+	C.fftw_execute(plan.cPlan)
+}
+
+func DCT32(samples []float64) []float64 {
+	output := make([]float64, 32)
+	_ = copy(output, samples)
+
+	in := NewArray(output)
+	plan := NewPlan(32, in, in, FFTWREDFT10, FFTWESTIMATE)
+	ExecutePlan(plan)
+
+	return output
+}
---
+++ hls/subband.go
@@ -0,0 +1,47 @@
+package hls
+
+import (
+	"math"
+	"math/big"
+	"streaming/fftw"
+)
+
+// 32-point DCT
+func DCT32(samples []float64) []float64 {
+	var bins []float64
+	for j := 0; j < 32; j++ {
+		binWithPrec := big.NewFloat(0).SetPrec(100)
+		for k := 0; k < 32; k++ {
+			sample := samples[k]
+			phase := math.Pi * float64(j) * (float64(2*k) + 1) / 64
+			filter := math.Cos(phase)
+			binWithPrec.Add(
+				binWithPrec,
+				big.NewFloat(0).SetPrec(100).
+					Mul(
+						big.NewFloat(sample).SetPrec(100),
+						big.NewFloat(filter).SetPrec(100),
+					),
+			)
+		}
+
+		bin, _ := binWithPrec.Mul(
+			binWithPrec,
+			big.NewFloat(2).SetPrec(100),
+		).Float64()
+		bins = append(bins, bin)
+	}
+
+	return bins
+}
+
+func DCT32ByFFTW(samples []float64) []float64 {
+	output := make([]float64, 32)
+	_ = copy(output, samples)
+
+	in := fftw.NewArray(output)
+	plan := fftw.NewPlan(32, in, in, fftw.FFTWREDFT10, fftw.FFTWESTIMATE)
+	fftw.ExecutePlan(plan)
+
+	return output
+}
