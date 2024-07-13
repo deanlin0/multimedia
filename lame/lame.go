b201666deb1d48dc9597c71e025b8932a9ab3181
Dean Lin <richer6605@gmail.com>
2024-07-13 15:51:41 +0800

    Remove unused LAME bindings

--- lame/lame.go
+++ lame/lame.go
@@ -15,36 +15,6 @@ package lame
 import "C"
 import "unsafe"
 
-// type LameContext struct {
-// 	cContext *C.lame_global_flags
-// }
-
-// func (ctx *LameContext) Output() []float64 {
-// 	xr := ctx.cContext.internal_flags.l3_side.tt[0][0].xr
-// 	output := make([]float64, 576)
-// 	for i := range xr {
-// 		output[i] = float64(xr[i])
-// 	}
-// 	return output
-// }
-
-// func AddEncodeDelay(data []float32, delay int) []float32 {
-// 	data = append(make([]float32, delay), data...)
-// 	return data
-// }
-
-// func NewLameContext() *LameContext {
-// 	cContext := C.lame_init()
-// 	_ = C.lame_init_params(cContext)
-// 	cContext.internal_flags.cfg.channels_in = 1
-// 	cContext.internal_flags.cfg.channels_out = 1
-// 	return &LameContext{cContext: cContext}
-// }
-
-// func ExecuteMDCTWithSubband(ctx *LameContext, inLeft, inRight *Array) {
-// 	C.mdct_sub48(ctx.cContext.internal_flags, inLeft.cArray, inRight.cArray)
-// }
-
 type Array struct {
 	cArray *C.float
 }
